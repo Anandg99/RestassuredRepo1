7d4f839f5d14d9f1164606a1fa6fd9a1feded72a
Ananda Babu Gummadilli <Anand.Gummadilli@neudesic.com>
2024-07-03 11:25:41 +0530

    Added class for enums

---
+++ RestAssured/src/test/java/Day1Learning/clsEnums.java
@@ -0,0 +1,16 @@
+package Day1Learning;
+import org.testng.annotations.Test;
+public class clsEnums {
+	enum Level {
+	    LOW,
+	    MEDIUM,
+	    HIGH
+	  }
+	@Test
+	public void LearnEnums()
+	{
+		Level _level=Level.LOW;
+		System.out.println("Level is:" + _level);
+		
+	}
+}
