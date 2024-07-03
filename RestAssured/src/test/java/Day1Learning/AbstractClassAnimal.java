16a6b921df54c7d6f424cf91aad3c58d4bb6d909
Ananda Babu Gummadilli <Anand.Gummadilli@neudesic.com>
2024-07-03 11:08:27 +0530

    added abstarct classes

---
+++ RestAssured/src/test/java/Day1Learning/AbstractClassAnimal.java
@@ -0,0 +1,11 @@
+package Day1Learning;
+
+public abstract class AbstractClassAnimal 
+{
+	public abstract void animalSound();
+
+	public void sleep() 
+	{
+		System.out.println("Sleep method non abtsract");
+	}
+}
